ef7a340bffa6628a248eb1ca002e46d9b451d107
Abhi <abhisheksinghmuz1999@gmail.com>
2024-05-17 16:11:50 +0530

    latest

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://127.0.0.1:27017/hotelsu';
+//const uri = 'mongodb://127.0.0.1:27017/hotelsu';
 //const uri = 'mongodb://localhost:27017/hotels';
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURL='mongodb+srv://abhisheksinghmuz1999:76jR5N0j3h9zCVbD@cluster0.o9bq1nq.mongodb.net/';
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error(err));
+  //vgjhfjkghftk
